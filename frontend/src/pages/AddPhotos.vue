1839aedff0767c9da9a414b2da67c2a4be8e0cdc
Zane <semogenaz@gmail.com>
2025-08-21 12:40:15 +1000

    Attempt to route from addItem to addPhotos

---
+++ frontend/src/pages/AddPhotos.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Item: {{ id }}</h1>
+    </div>
+</template>
+
+<script setup>
+    import { useRoute } from 'vue-router'
+    import { ref } from 'vue';
+
+    const route = useRoute();
+    const id = ref(route.params.id);
+    
+</script>
